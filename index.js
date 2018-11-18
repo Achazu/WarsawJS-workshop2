01071e8c571c44129fa5ed7a871d725d77f7db49
Luke_t <lukasz.tylke@gmail.com>
2018-11-18 15:21:01 +0100

    basic features

---
+++ index.js
@@ -0,0 +1,64 @@
+
+const images = ['images/confront.png', 'images/warsawjs.png'];
+const gallery = document.getElementById('images')
+const fav = document.getElementById('show-favourites')
+
+class Image {
+	constructor(path){
+		this.path = path
+	}
+
+	show(){
+		this.newImage = document.createElement('img');
+		
+		this.newImage.setAttribute('src', this.path);	
+		gallery.appendChild(this.newImage);
+		
+		this.newImage.addEventListener('click', () => this.toggleFavourite());
+	}
+
+	hide(){
+		this.newImage.style.display = 'none';
+	}
+
+	isFavourite(){
+		return this.newImage.classList.contains('image--favourite')
+	}
+
+	toggleFavourite(){
+		this.newImage.classList.toggle('image--favourite');
+	}
+}
+
+function showFav(){
+	galleryElements.filter(photo => !photo.isFavourite(photo)).forEach(photo => photo.hide());
+}
+
+const galleryElements = images.map(photo => new Image(photo))
+galleryElements.forEach(photo => photo.show());
+
+fav.addEventListener('click', showFav)
+
+
+
+
+
+
+// images.forEach( photo => {
+
+// 	const singlePhoto = new Image(photo)
+// 	singlePhoto.show()
+
+// })
+
+// function showFavourite(e){
+// 	// !this.classList.contains('image--favourite') ?
+// 	// this.classList.add('image--favourite') :
+// 	// this.classList.remove('image--favourite');
+
+// 	if(e.target.tagName === 'IMG'){
+// 		e.target.classList.toggle('image--favourite')
+// 	}
+// }
+
+// image.addEventListener('click', showFavourite);
